0fee5de5c6390766f337dcf5316be41a864469f5
Brandon Lee <blee2125@gmail.com>
2023-12-04 12:14:28 -0700

    add text input field

--- src/components/inputs/ExpectedValue.js
+++ src/components/inputs/ExpectedValue.js
@@ -1,8 +1,27 @@
+import React, { useState, useEffect } from "react";
 
-function ExpectedValue() {
+function ExpectedValue(props) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (val) => {
+    setValue(val);
+    //onChange();
+  };
+
+  useEffect(() => {
+    props.setExpected(value)
+    // eslint-disable-next-line
+  }, [value])
+
   return (
     <>
       ExpectedValue
+      <form>
+        <input
+          type="text"
+          onChange={e => handleChange(e.target.value)}
+        />
+      </form>
     </>
   );
 }
